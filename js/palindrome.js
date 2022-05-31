2e41980691115d3cbfa5af7818ca310f52912ae1
Michael Echevarria <michael.echevarria@live.com>
2022-05-31 09:49:15 -0500

    updated code for js

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,12 @@
 exports.palindrome = function(word) {
+    word = word.toString();
+    let regWord = word.match(/[a-z0-9]+/gi);
+    let forwardWord = regWord.join("").toUpperCase();
+    let reversedWord = regWord.join("").split("").reverse().join("").toUpperCase();
 
+    if (forwardWord === reversedWord) {
+        return true;
+    } else {
+        return false;
+    }
 };
--- js/palindromeSpec.js
+++ js/palindromeSpec.js
@@ -1,5 +1,3 @@
-// Can you translate this driver code to unit tests?
-
 var pal = require("./palindrome");
 
 console.log(pal.palindrome('racecar') === true);
